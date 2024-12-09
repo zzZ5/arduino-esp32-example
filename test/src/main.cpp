f699b19919feebc2c4f7856214578ef83befce60
zzZ5 <baoju_liu@foxmail.com>
2024-12-09 10:16:06 +0800

    add test project

---
+++ test/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  printf("Result: %d\n", result);
+}
+
+void loop() {
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
