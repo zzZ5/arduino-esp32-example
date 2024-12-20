c05262e9b90ed6c093596cad89273e6cc6ff6966
zzZ5 <baoju_liu@foxmail.com>
2024-12-20 17:48:21 +0800

    test pin2

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,19 +1,33 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int pin = 2;  // 假设我们使用 GPIO 2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  printf("Result: %d\n", result);
+  // 初始化串口通讯
+  Serial.begin(115200);
+
+  // 初始化GPIO引脚为输出模式
+  pinMode(pin, OUTPUT);
+
+  Serial.println("请随时输入 '1' 设置为高电平，'0' 设置为低电平");
 }
 
 void loop() {
+  // 检查是否有串口输入
+  if (Serial.available() > 0) {
+    char input = Serial.read();  // 读取输入的字符
 
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // 根据输入字符控制 GPIO 电平
+    if (input == '1') {
+      digitalWrite(pin, HIGH);  // 设置为高电平
+      Serial.println("GPIO 设置为高电平");
+    }
+    else if (input == '0') {
+      digitalWrite(pin, LOW);   // 设置为低电平
+      Serial.println("GPIO 设置为低电平");
+    }
+    else {
+      Serial.println("无效输入，请输入 '1' 或 '0'");
+    }
+  }
 }
